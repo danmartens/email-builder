2713a8caf123fe70f67373f7c843cf0a852d6e72
Dan Martens <dan@friendsoftheweb.com>
2020-03-24 18:40:48 -0400

    Set up ws server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import posthtml, { PostHTML } from "posthtml";
-import inlineCSS from "posthtml-inline-css";
-import prettier from "prettier";
-import Handlebars from "handlebars";
+import posthtml, { PostHTML } from 'posthtml';
+import path from 'path';
+import fs from 'fs';
+import inlineCSS from 'posthtml-inline-css';
+import prettier from 'prettier';
+import Handlebars from 'handlebars';
+import express from 'express';
+import WebSocket from 'ws';
 
 interface BoxValues {
   top: number;
@@ -11,8 +15,8 @@ interface BoxValues {
 }
 
 const parseBoxValues = (values: string): BoxValues => {
-  if (typeof values === "string") {
-    const parsedValues = values.split(/\s+/).map(value => parseInt(value));
+  if (typeof values === 'string') {
+    const parsedValues = values.split(/\s+/).map((value) => parseInt(value));
 
     switch (parsedValues.length) {
       case 4:
@@ -45,29 +49,29 @@ const parseBoxValues = (values: string): BoxValues => {
   }
 };
 
-const paddingElement = tree => {
-  tree.match({ tag: "padding" }, node => {
+const paddingElement = (tree) => {
+  tree.match({ tag: 'padding' }, (node) => {
     return {
-      tag: "table",
+      tag: 'table',
       content: [
         {
-          tag: "tr",
+          tag: 'tr',
           content: [
-            { tag: "td", attrs: { colspan: "3", height: node.attrs?.top } }
+            { tag: 'td', attrs: { colspan: '3', height: node.attrs?.top } }
           ]
         },
         {
-          tag: "tr",
+          tag: 'tr',
           content: [
-            { tag: "td", attrs: { width: node.attrs?.left } },
-            { tag: "td", content: node.content },
-            { tag: "td", attrs: { width: node.attrs?.right } }
+            { tag: 'td', attrs: { width: node.attrs?.left } },
+            { tag: 'td', content: node.content },
+            { tag: 'td', attrs: { width: node.attrs?.right } }
           ]
         },
         {
-          tag: "tr",
+          tag: 'tr',
           content: [
-            { tag: "td", attrs: { colspan: "3", height: node.attrs?.bottom } }
+            { tag: 'td', attrs: { colspan: '3', height: node.attrs?.bottom } }
           ]
         }
       ]
@@ -75,23 +79,23 @@ const paddingElement = tree => {
   });
 };
 
-const paddingAttribute = tree => {
-  tree.match({ attrs: { padding: /\d+( \d+){0,3}/ } }, node => {
+const paddingAttribute = (tree) => {
+  tree.match({ attrs: { padding: /\d+( \d+){0,3}/ } }, (node) => {
     const { top, left, bottom, right } = parseBoxValues(node.attrs.padding);
 
     return {
-      tag: "padding",
+      tag: 'padding',
       attrs: { top, left, bottom, right },
       content: { ...node, attrs: { ...node.attrs, padding: undefined } }
     };
   });
 };
 
-const tableElement = tree => {
-  tree.match({ tag: "table" }, node => {
+const tableElement = (tree) => {
+  tree.match({ tag: 'table' }, (node) => {
     return {
       ...node,
-      attrs: { border: "0", cellspacing: "0", cellpadding: "0", ...node.attrs }
+      attrs: { border: '0', cellspacing: '0', cellpadding: '0', ...node.attrs }
     };
   });
 };
@@ -100,15 +104,63 @@ const render = async (html, data = {}) => {
   const template = Handlebars.compile(html);
 
   const result = await posthtml([
-    inlineCSS("p { color:red; }"),
+    inlineCSS('p { color:red; }'),
     paddingAttribute,
     paddingElement,
     tableElement
   ]).process(template(data));
 
-  console.log(prettier.format(result.html, { parser: "html" }));
+  return prettier.format(result.html, { parser: 'html' });
 };
 
-const html = `<p padding="20 30 45 3">{{hello}}</p>`;
+const app = express();
 
-render(html, { hello: "world" });
+app.get('/', (req, res) => {
+  const html = fs
+    .readFileSync(path.join(__dirname, 'emails/template.hbs'))
+    .toString();
+
+  render(html).then((data) => {
+    res.send(`
+      <html>
+        <head>
+          <script>
+            const socket = new WebSocket('ws://localhost:8080');
+
+            socket.addEventListener('message', () => {
+              location.reload();
+            });
+          </script>
+        </head>
+
+        <body>${data}</body>
+      </html>
+    `);
+  });
+});
+
+import chokidar from 'chokidar';
+
+const watcher = chokidar.watch(path.join(__dirname, 'emails/*.hbs'));
+
+const server = new WebSocket.Server({
+  port: 8080
+});
+
+const connections = new Set<WebSocket>();
+
+server.on('connection', (connection) => {
+  connections.add(connection);
+});
+
+watcher.on('change', (changedPath) => {
+  for (const connection of connections) {
+    connection.send(
+      JSON.stringify({
+        path: changedPath.replace(path.join(__dirname, 'emails'), '')
+      })
+    );
+  }
+});
+
+app.listen(3000, () => console.log('Now listening on port 3000...'));
