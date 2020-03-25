d95b151c3f8f98e0b0d3a4d799b41d11013aa414
Dan Martens <dan@friendsoftheweb.com>
2020-03-25 14:17:44 -0400

    Add list of emails to root

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,14 +3,41 @@ import fs from 'fs';
 import express from 'express';
 import WebSocket from 'ws';
 import chokidar from 'chokidar';
+import Handlebars from 'handlebars';
 import { renderEmail } from '../renderEmail';
 
 export const server = () => {
   const app = express();
 
   app.get('/', (req, res) => {
+    const template = Handlebars.compile(
+      fs.readFileSync(path.resolve(__dirname, './views/index.hbs')).toString()
+    );
+
+    res.send(
+      template({
+        emails: fs
+          .readdirSync(path.resolve(__dirname, '../emails'))
+          .map((item) => {
+            return {
+              name: item
+            };
+          })
+      })
+    );
+  });
+
+  app.get('/:name', (req, res) => {
     const html = fs
-      .readFileSync(path.resolve(__dirname, '../emails/template.hbs'))
+      .readFileSync(
+        path.resolve(
+          __dirname,
+          `../emails/${req.params.name.replace(
+            /[^a-z0-9\-_]/gi,
+            ''
+          )}/template.hbs`
+        )
+      )
       .toString();
 
     renderEmail(html).then((data) => {
@@ -18,7 +45,7 @@ export const server = () => {
     });
   });
 
-  const watcher = chokidar.watch(path.resolve(__dirname, '../emails/*.hbs'));
+  const watcher = chokidar.watch(path.resolve(__dirname, '../emails/**/*.hbs'));
 
   const server = new WebSocket.Server({
     port: 8080
