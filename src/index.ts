52df47d44489c006832bd7dc3d9b3cd8a8b6ef23
Dan Martens <dan@friendsoftheweb.com>
2020-04-09 20:12:37 -0400

    Add commander for arg handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
+#! /usr/bin/env node
+
 require('dotenv').config();
 
+import program from 'commander';
 import { server } from './server';
 
-server('development');
+program.version(require('../package.json')['version']);
+
+program
+  .command('server')
+  .alias('s')
+  .description('starts production server')
+  .action(() => {
+    server('production');
+  });
+
+program
+  .command('develop')
+  .alias('d')
+  .description('starts development server')
+  .action(() => {
+    server('development');
+  });
+
+program.parse(process.argv);
